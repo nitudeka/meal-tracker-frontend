aae20b88407fee6c04274e093bcaf6e743de5422
nitudeka <iamnitudeka@gmail.com>
2025-08-01 00:02:17 +0530

    add date-picker, calendar and popover components

---
+++ src/components/ui/date-picker.jsx
@@ -0,0 +1,37 @@
+import * as React from "react"
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+ 
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+ 
+function DatePicker({ date, setDate }) {
+  const handleDateChange = (date) => {
+    setDate(date)
+  }
+ 
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          data-empty={!date}
+          className="data-[empty=true]:text-muted-foreground w-full justify-start text-left font-normal"
+        >
+          <CalendarIcon />
+          {date ? format(date, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar mode="single" selected={date} onSelect={handleDateChange} />
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export default DatePicker;
