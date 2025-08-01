c98fac72207ea2a07d0a2807941c1b31c5e57f07
nitudeka <iamnitudeka@gmail.com>
2025-08-01 14:15:30 +0530

    add button to add diet entry

--- src/features/diet/history/index.jsx
+++ src/features/diet/history/index.jsx
@@ -1,11 +1,20 @@
 import NutritionTrends from "./NutrititionTrends";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
 
 const DietHistory = () => {
+    const navigate = useNavigate();
+
+    const handleAddEntry = () => {
+        navigate("/diet/add");
+    }
+
   return (
     <div className="p-6 h-full overflow-y-auto hide-scrollbar">
-      <div className="mb-8">
-        <h2 className="text-3xl font-bold text-gray-900">Diet History</h2>
+      <div className="mb-8 relative">
+        <h2 className="text-2xl font-bold text-gray-900">Diet History</h2>
         <p className="text-gray-600 mt-1">Track your nutrition trends and goals</p>
+        <Button className="absolute right-0 top-0" onClick={handleAddEntry}>+ Add Entry</Button>
       </div>
 
       <div className="max-w-4xl mx-auto">
